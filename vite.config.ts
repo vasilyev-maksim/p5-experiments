f8916b11f71445729ffe5dd609c295faeb7edace
Maksim Vasilyev <m.vasilyev@sweedpos.com>
2025-12-16 20:13:49 +0400

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/p5-experiments/",
-});
+  base: mode === "production" ? "/p5-experiments/" : "/",
+}));
